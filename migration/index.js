78d43af263518ac5c2bb8405a56d6753eda58ccf
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2024-06-02 09:11:05 +0700

    feat: migration create command

--- migration/index.js
+++ migration/index.js
@@ -1,6 +1,11 @@
 // @ts-check
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+import { existsSync, mkdirSync, readdirSync, writeFileSync } from 'node:fs';
+import { platform } from 'node:os';
 
 const [COMMAND, ARG1] = process.argv.splice(2);
+const SQL_DIR = new URL('sql', import.meta.url);
 
 switch (COMMAND) {
 	case 'create':
@@ -14,6 +19,8 @@ switch (COMMAND) {
 		process.exit(1);
 }
 
+if (!existsSync(SQL_DIR)) mkdirSync(SQL_DIR);
+
 /**
  * Create new migration file with given name
  *
@@ -24,7 +31,54 @@ function create(name) {
 		console.error(`invalid arg: migration name is required`);
 		process.exit(1);
 	}
-	console.log(`creating "${name}"...`);
+
+	const redirectStdout = platform() === 'win32' ? `> NUL 2>&1` : `> /dev/null 2>&1`;
+
+	/** @type {Set<string>} */
+	const existingFiles = new Set();
+
+	/** @param {string} branch */
+	function pushBranchFiles(branch) {
+		for (const file of execFmt(`git diff ${branch} --name-only ./migration/sql/`)) {
+			if (!file || !file.endsWith('.sql')) continue;
+			existingFiles.add(file.replace('migration/sql/', ''));
+		}
+	}
+
+	// check local files
+	console.log(`~ checking migration file on local directory...`);
+	for (const file of readdirSync(SQL_DIR)) {
+		if (!file.endsWith('.sql')) continue;
+		existingFiles.add(file);
+	}
+
+	// check local branches
+	console.log(`~ checking migration file on local branches...`);
+	for (const branch of execFmt(`git for-each-ref --format="%(refname:short)" refs/heads/`)) {
+		pushBranchFiles(branch);
+	}
+
+	// check remote branches
+	console.log(`~ checking migration file on remotes branches...`);
+	execSync(`git fetch --all ${redirectStdout}`);
+	for (const ref of execFmt(`git for-each-ref --format="%(refname:short)" refs/remotes/*/*`)) {
+		const [remote, branch] = ref.split('/');
+		if (!branch) continue;
+
+		const tempBranch = `temp__${branch}__${randomUUID().slice(0, 8)}`;
+		execSync(`git fetch ${remote} ${branch}:${tempBranch} ${redirectStdout}`);
+		pushBranchFiles(tempBranch);
+		execSync(`git branch -D ${tempBranch} ${redirectStdout}`);
+	}
+
+	// create migration file
+	const migrationFiles = [...existingFiles].filter(Boolean);
+	const lastMigrationId = Number(migrationFiles.sort().at(-1)?.substring(0, 4) || '0000');
+	const migrationName = `${String(lastMigrationId + 1).padStart(4, '0')}_${name}.sql`;
+	const newMigrationFile = new URL(`${SQL_DIR}/${migrationName}`);
+
+	writeFileSync(newMigrationFile, `select now();\n`);
+	console.log(`> created: "${migrationName}"`);
 }
 
 /**
@@ -34,4 +88,11 @@ async function migrate() {
 	console.log(`running migration...`);
 }
 
-export {};
+/**
+ * @param {string} command
+ * @returns {string[]}
+ */
+function execFmt(command) {
+	const output = execSync(command);
+	return output.toString().trim().split('\n');
+}
